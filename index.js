58f0c7d4c05e47541e66fa9002517f225dda6fbb
panpa62 <142426986+panpa62@users.noreply.github.com>
2023-10-04 12:07:27 -0400

    Modified json.data & date format

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const COHORT = "2309-FSA-ET-WEB-FT-SF";
-//const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`
-const API_URL = 'https://fsa-crud-2aa9294fe819.herokuapp.com/api/2309-FSA-ET-WEB-FT-SF/events'
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`
+//const API_URL = 'https://fsa-crud-2aa9294fe819.herokuapp.com/api/2309-FSA-ET-WEB-FT-SF/events'
 const state = {
-    parties: []
+    partys: []
 }
 
 const partyList = document.querySelector('#parties')
@@ -14,28 +14,34 @@ async function getParties(){
     try{
         const response = await fetch(API_URL);
         const json = await response.json();
-        state.parties = json;
+        state.partys = json.data;
+        //console.log("get parties: " + JSON.stringify(state.partys))
+        //console.log("state.partys.length = " + Object.keys(state.partys).length)
 
     }catch(err){
-        console.log(err)
+        console.log(err);
     }
 }
 
 function renderParties(){
-    if(!state.parties.length){
+    //console.log("state.partys.length = " + Object.keys(state.partys).length)
+    if(!state.partys.length){
         partyList.innerHTML = '<li>No parties found.</li>';
+        //console.log("was at renderParties with no parties found")
         return;
     }
 
-    const partyCards = state.parties.map((party) => {
+    const partyCards = state.partys.map((party) => {
         const partyCard = document.createElement("li");
         partyCard.classList.add("party");
+       
         partyCard.innerHTML = `
-        <h2>${party.title}</h2>
-        <p>${party.location}</p>
-        <p>${party.date}</p>
-        <p>${party.description}</p>
+        <h2>${party.name}</h2>
+        <h3>${party.location}</h3>
+        <h3>${party.date}</h3>
+        <h3>${party.description}</h3>
         `;
+        
 
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "Delete Party";
@@ -48,19 +54,20 @@ function renderParties(){
 
 async function render(){
     await getParties();
+    console.log("was at render after getParties")
     renderParties();
+    console.log("was at render after renderParties")
 }
 render();
 
-async function createParty(title, location, date, description){
+async function createParty(name, location, date, description) {
     try{
         const response = await fetch(API_URL, {
             method: "POST",
             headers: { "Content-Type": "application/json"},
-            body: JSON.stringify({title, location, date, description}),
+            body: JSON.stringify({name, location, date, description}),
         });
         const json = await response.json();
-        console.log(json)
         if(json.error){
             throw new Error(json.message);
         }
@@ -72,25 +79,26 @@ async function createParty(title, location, date, description){
 
 async function addParty(event){
     event.preventDefault();
+    const date = new Date(addPartyForm.date.value)
     await createParty(
-        addPartyForm.title.value,
+        addPartyForm.name.value,
         addPartyForm.location.value,
-        addPartyForm.date.value,
+        date.toISOString(),
         addPartyForm.description.value
     );
 
-    addPartyForm.title.value = '';
+    addPartyForm.name.value = '';
     addPartyForm.location.value = '';
     addPartyForm.date.value = '';
     addPartyForm.description.value = '';
 }
 
-async function updateParty(id, title, location, date, description){
+async function updateParty(id, name, location, date, description){
     try{
         const response = await fetch(`${API_URL}/${id}`, {
             method: "PUT",
             headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({title, location, date, description}),
+            body: JSON.stringify({name, location, date, description}),
         });
         const json = await response.json();
         if(json.error){
